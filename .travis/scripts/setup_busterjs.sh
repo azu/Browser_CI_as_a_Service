534818ea67b741c13ee9ca6288c799b25be3ae61
azu <azuciao@gmail.com>
2013-03-20 19:40:06 +0900

    update buster.JS setup

--- .travis/scripts/setup_busterjs.sh
+++ .travis/scripts/setup_busterjs.sh
@@ -1,9 +1,8 @@
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
-sleep 3
+#!/bin/sh
+
 node_modules/.bin/buster-server &
-sleep 3
+sleep 5
 firefox http://localhost:1111/capture &
-sleep 3
+sleep 5
 phantomjs node_modules/buster/script/phantom.js http://localhost:1111/capture &
-sleep 3
+sleep 5

d7362732c2d84d13ff9e904bd976e31258359b64
azu <azuciao@gmail.com>
2013-04-18 21:26:46 +0900

    fix Google-Chrome for Travis CI

--- .travis/scripts/setup_busterjs.sh
+++ .travis/scripts/setup_busterjs.sh
@@ -10,6 +10,6 @@ phantomjs node_modules/buster/script/phantom.js http://localhost:1111/capture &
 sleep 5
 which google-chrome > /dev/null 2>&1
 if [ $? -eq 0 ] ; then # Google Chrome
-    google-chrome --no-default-browser-check --no-first-run --disable-default-apps http://localhost:1111/capture &
+    google-chrome --no-default-browser-check --no-first-run --disable-default-apps --no-sandbox http://localhost:1111/capture &
     sleep 5
 fi
